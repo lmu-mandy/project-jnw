4416e2f44ff5fd70cb096f88bce024cf114d0e44
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-11-21 23:41:55 -0800

    return content of email but doesn't work for forwards

--- nn.py
+++ nn.py
@@ -25,45 +25,50 @@ def load_vocab(text):
 
 
 train_data = []
-df = pd.read_csv("data/labeled_data.csv")
+df = pd.read_csv("emails.csv", nrows=500)
 for index, row in df.iterrows():
-    train_data.append((row['tweet'], row['class']))
-tok_to_ix = load_vocab(train_data)
+    train_data.append((row['message'].splitlines()[16:]))
+# tok_to_ix = load_vocab(train_data)
 
-emb_dim = 5
-num_classes = 3
-learning_rate = 0.01
-model = Net(len(tok_to_ix), emb_dim, num_classes)
-optimizer = optim.SGD(model.parameters(), lr=learning_rate)
-loss_fn = nn.BCELoss()
+updated_data = train_data
+train_data[:] = [' '.join(x).split('.') for x in train_data]
+train_data[:] = [[elem for elem in x if elem.strip()] for x in train_data]
+train_data[:] = [[elem.strip() for elem in x] for x in train_data]
 
-n_epochs = 5
-for epoch in range(n_epochs):
-    model.train()
-    for text, label in train_data:
-        x = [tok_to_ix[tok] for tok in text.split()]
-        x_train_tensor = torch.LongTensor(x)
-        if label == 2:
-            y_train_tensor = torch.Tensor([0, 0, 1])
-        elif label == 1:
-            y_train_tensor = torch.Tensor([0, 1, 0])
-        else:
-            y_train_tensor = torch.Tensor([1, 0, 0])
-        # y_train_tensor = torch.Tensor([label])
-        pred_y = model(x_train_tensor)
-        loss = loss_fn(pred_y, y_train_tensor)
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-    print("\nEpoch:", epoch)
-    print("Training loss:", loss.item())
+# for str_lists in train_data:
+#   str_lists = [elem for elem in str_lists if elem.strip()]
+#   print(str_lists)
+  # if '' in str_lists:
+  #   str_lists.remove("")
+  # if ' ' in str_lists:
+  #   str_lists.remove(" ")
 
-# y_test = 'sad face'
-# with torch.no_grad():
-#     model.eval()
-#     x = [tok_to_ix[tok] for tok in y_test.split() if tok in tok_to_ix]
-#     x_test = torch.LongTensor(x)
-#     pred_y_test = model(x_test)
+print(train_data)
 
-#     print('Test sentence:', y_test)
-#     print('Predicted label:', pred_y_test.data.numpy())
+# emb_dim = 5
+# num_classes = 3
+# learning_rate = 0.01
+# model = Net(len(tok_to_ix), emb_dim, num_classes)
+# optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+# loss_fn = nn.BCELoss()
+
+# n_epochs = 5
+# for epoch in range(n_epochs):
+#     model.train()
+#     for text, label in train_data:
+#         x = [tok_to_ix[tok] for tok in text.split()]
+#         x_train_tensor = torch.LongTensor(x)
+#         if label == 2:
+#             y_train_tensor = torch.Tensor([0, 0, 1])
+#         elif label == 1:
+#             y_train_tensor = torch.Tensor([0, 1, 0])
+#         else:
+#             y_train_tensor = torch.Tensor([1, 0, 0])
+#         # y_train_tensor = torch.Tensor([label])
+#         pred_y = model(x_train_tensor)
+#         loss = loss_fn(pred_y, y_train_tensor)
+#         loss.backward()
+#         optimizer.step()
+#         optimizer.zero_grad()
+#     print("\nEpoch:", epoch)
+#     print("Training loss:", loss.item())
