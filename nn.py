71ab8ebea200184629978777578958b33ed46861
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-11-22 13:24:43 -0800

    small updates

--- nn.py
+++ nn.py
@@ -42,8 +42,8 @@ train_data = list(itertools.chain.from_iterable(train_data))
 
 rand_word = random.choice(train_data[0].split())
 
-start_index = train_data[0].find(rand_word)
-end_index = start_index + len(rand_word)
+# start_index = train_data[0].find(rand_word)
+# end_index = start_index + len(rand_word)
 # print(rand_word)
 # print(train_data[0].replace(rand_word, '???', 1))
 
@@ -54,34 +54,34 @@ for sentence in train_data:
   masked_sent = re.sub(re.escape(rand_word), '???', sentence, count=1)
   tuples_list.append((rand_word, masked_sent))
 
-# print(tuples_list[0:10])
+print(tuples_list[0:5])
 
-tok_to_ix = load_vocab(tuples_list)
+# tok_to_ix = load_vocab(tuples_list)
 
-emb_dim = 5
-num_classes = 3
-learning_rate = 0.01
-model = Net(len(tok_to_ix), emb_dim, num_classes)
-optimizer = optim.SGD(model.parameters(), lr=learning_rate)
-loss_fn = nn.BCELoss()
+# emb_dim = 5
+# num_classes = 3
+# learning_rate = 0.01
+# model = Net(len(tok_to_ix), emb_dim, num_classes)
+# optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+# loss_fn = nn.BCELoss()
 
-n_epochs = 5
-for epoch in range(n_epochs):
-    model.train()
-    for label, text in tuples_list:
-        x = [tok_to_ix[tok] for tok in text.split()]
-        x_train_tensor = torch.LongTensor(x)
-        # if label == 2:
-        #     y_train_tensor = torch.Tensor([0, 0, 1])
-        # elif label == 1:
-        #     y_train_tensor = torch.Tensor([0, 1, 0])
-        # else:
-        #     y_train_tensor = torch.Tensor([1, 0, 0])
-        y_train_tensor = torch.Tensor([label])
-        pred_y = model(x_train_tensor)
-        loss = loss_fn(pred_y, y_train_tensor)
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-    print("\nEpoch:", epoch)
-    print("Training loss:", loss.item())
+# n_epochs = 5
+# for epoch in range(n_epochs):
+#     model.train()
+#     for label, text in tuples_list:
+#         x = [tok_to_ix[tok] for tok in text.split()]
+#         x_train_tensor = torch.LongTensor(x)
+#         # if label == 2:
+#         #     y_train_tensor = torch.Tensor([0, 0, 1])
+#         # elif label == 1:
+#         #     y_train_tensor = torch.Tensor([0, 1, 0])
+#         # else:
+#         #     y_train_tensor = torch.Tensor([1, 0, 0])
+#         y_train_tensor = torch.Tensor([label])
+#         pred_y = model(x_train_tensor)
+#         loss = loss_fn(pred_y, y_train_tensor)
+#         loss.backward()
+#         optimizer.step()
+#         optimizer.zero_grad()
+#     print("\nEpoch:", epoch)
+#     print("Training loss:", loss.item())
