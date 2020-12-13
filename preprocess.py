60b037675117ed69799928fe7fc4b124c254ad28
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-12-13 13:29:41 -0800

    add comments

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,9 @@ import pandas as pd
 import itertools
 import string
 
+# Program to preprocess (remove whitespace, headers, and all lines of text that only contain letters)
+# and then split up the preprocessed data into 3 separate csv files (train, test, evaluation)
+
 # Read in 10,000 lines of the email data (start at row 16 to avoid reading in headers)
 train_data = []
 df = pd.read_csv("emails.csv", nrows=10000, usecols=range(2))
