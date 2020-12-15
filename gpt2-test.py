fd5039141d91a9b187106b1172b77a9c1bb7d411
Will DiBiagio <wdibiagio@gmail.com>
2020-12-15 14:54:17 -0800

    refactor test data import and testing loop

--- gpt2-test.py
+++ gpt2-test.py
@@ -10,6 +10,10 @@ import csv
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
+TEST_DATA_SOURCE = "./data/test.csv"
+
+test_data = pd.read_csv(TEST_DATA_SOURCE, quotechar='`')
+
 df = pd.read_csv("data/test.csv", quotechar='`')
 sentences = []
 for i, row in df.iterrows():
@@ -27,20 +31,19 @@ avg_len = 0
 with open('gpt2_results.csv', 'w') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['accuracy', 'avg_valid_rank', 'rank', 'avg_valid_len', 'len'])
-    for sent in sentences:
-        text = sent[0]
+    for mask, text in zip(test_data['mask'], test_data['text']):
         indexed_tokens = tokenizer.encode(text)
         tokens_tensor = torch.tensor([indexed_tokens])
 
         with torch.no_grad():
             outputs = model(tokens_tensor)
             predictions = outputs[0]
+  
         top_k = 10
         predicted_index = torch.argmax(predictions[0, -1, :]).item()
         idx = predictions[0, -1, :].topk(top_k).indices.tolist()
-        next_words = {tokenizer.decode(
-            [j]).strip(): i for i, j in enumerate(idx)}
-        next_word_rank = next_words[sent[1]] if sent[1] in next_words else -1
+        next_words = {tokenizer.decode([j]).strip(): i for i, j in enumerate(idx)}
+        next_word_rank = next_words[mask] if mask in next_words else -1
         total += 1
         if next_word_rank >= 0:
             valid += 1
