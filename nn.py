24ac3245531b2571e8c03c110270c1a55075ee7d
JigarSwam <jswaminarayan@gmail.com>
2020-12-15 14:45:31 -0800

    add function comments

--- nn.py
+++ nn.py
@@ -19,6 +19,7 @@ test_data = pd.read_csv(TEST_DATA_SOURCE, quotechar='`')
 
 # Feed-Forward Network class constructor
 
+
 class Net(nn.Module):
     def __init__(self, num_words, emb_dim, num_y):
         super().__init__()
@@ -30,6 +31,8 @@ class Net(nn.Module):
         embeds = torch.mean(self.emb(text), dim=0)
         return self.sigmoid(self.linear(embeds))
 
+# Loading data and splitting up words and masked words
+
 
 def load_vocab_masks(text):
     word_to_ix = {}
@@ -37,10 +40,11 @@ def load_vocab_masks(text):
     ix_to_mask = {}
     for mask, text in zip(train_data['mask'], train_data['text']):
         for word in text.split(' '):
-          word_to_ix.setdefault(word, len(word_to_ix))
+            word_to_ix.setdefault(word, len(word_to_ix))
         mask_to_ix.setdefault(mask, len(mask_to_ix))
         ix_to_mask[mask_to_ix[mask]] = mask
     return word_to_ix, mask_to_ix, ix_to_mask
+
 
 word_to_ix, mask_to_ix, ix_to_mask = load_vocab_masks(train_data)
 
@@ -70,6 +74,14 @@ for epoch in range(n_epochs):
     print("\nEpoch:", epoch)
     print("Training loss:", loss.item())
 
+'''
+Function for evaluating the model
+@params
+masks: list of potential masked words
+texts: sample unfinished sentences with a missing word
+'''
+
+
 def evaluate(model, masks, texts):
     valid = 0
     total = 0
@@ -78,16 +90,17 @@ def evaluate(model, masks, texts):
         y_test = mask
         with torch.no_grad():
             model.eval()
-            x = [word_to_ix[word] for word in y_test.split() if word in word_to_ix]
+            x = [word_to_ix[word]
+                 for word in y_test.split() if word in word_to_ix]
             x_test = torch.LongTensor(x)
             pred_y_test = model(x_test)
             values, mask_indices = pred_y_test.topk(top_k)
             for i, mask_index in enumerate(mask_indices):
                 pred_word = ix_to_mask[mask_index.item()]
                 if mask == pred_word:
-                  valid += 1
-                  break
-            total +=  1
+                    valid += 1
+                    break
+            total += 1
     print(f"Accuracy (k={top_k}): {valid/total}")
 
 
